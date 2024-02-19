6fec93f65ead3a80cccd091a4caa46c99137f792
KaynoxDev <r.chauvard18@gmail.com>
2024-02-19 23:30:38 +0100

    structure html

---
+++ src/components/Myproject.vue
@@ -0,0 +1,24 @@
+    <script setup>
+
+    </script>
+
+    <template>
+        <div id="project">
+        <section id="myProject" ref="myProject">
+        <h2>Mes Projets</h2>
+        <article>
+            <ul ref="projectList" @wheel="onWheel">
+    <li v-for="project in visibleProjects" :key="project.id">
+        <h3>{{ project.title }}</h3>
+        <img :src="project.imageSrc" alt="images de mes projets">
+    </li>
+    </ul>
+        </article>
+        </section>
+    </div>
+    </template>
+
+    <style scoped>
+
+    
+    </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <Header/>
   <aboutMe/>
+  <Myproject/>
 </template>
 
 <script setup>
 import Header from '../components/Header.vue';
 import aboutMe from '../components/aboutMe.vue';
+import Myproject from '../components/Myproject.vue';
 </script>
