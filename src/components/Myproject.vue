72165eaa1675f2e234190e90be3fc0f16f2bc652
KaynoxDev <r.chauvard18@gmail.com>
2024-02-19 23:31:56 +0100

    structure css mes projets

--- src/components/Myproject.vue
+++ src/components/Myproject.vue
@@ -20,5 +20,75 @@
 
     <style scoped>
 
+    section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #043444;  
+    box-shadow: 5px 5px 20px #bcc9ce;
+    width: 80%;
+    border-radius: 1%;
     
+    
+    }
+
+    section h2 ,section p{
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+    color: var(--secondary-color);
+    letter-spacing: 2px;
+    color: #04a3e3;
+    }
+
+    section h2 {
+        text-decoration: underline;
+    }
+
+    #myProject{
+    display: flex ;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    margin-top: 100px;
+    }
+
+    #project {
+        display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 50px;
+    
+    }
+
+    article {
+        width: 100%;
+        color: var(--secondary-color);
+    }
+
+    ul {
+    display: flex;
+    justify-content: space-around;
+    list-style: none;
+    
+    }
+
+    li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 20px;
+    
+    margin: 10px;
+    
+    }
+
+    img {
+    width: 100%;
+    height: 500px;
+    border-radius: 10px;
+    }
+
+
     </style>
