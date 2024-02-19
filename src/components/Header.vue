0084f732e52851f960c229747befbd6d719a84e9
KaynoxDev <r.chauvard18@gmail.com>
2024-02-19 10:22:33 +0100

    Structure html du header

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import image from '../assets/img/logo.png'
+import { ref } from 'vue';
+
+const x = ref(0);
+const y = ref(0);
+
+const mouseMove = (event) => {
+  x.value = event.clientX / window.innerWidth;
+  y.value = event.clientY / window.innerHeight;
+};
+
+
+</script>
+
+<template>
+  <header>
+      <router-link to="/">
+      <img alt="logo de kaynoxdev" :src="image" @mousemove="mouseMove" :style="{ transform: 'translate(' + x * 50 + 'px, ' + y* 20 + 'px)' }"/>
+  </router-link>
+    
+    
+      <nav>
+        <a href="#" >Accueil</a>
+        <a :href="'#myHistory'">Présentation</a>
+        <a :href="'#myProject'">Mes projets</a>
+        <a :href="'#myContact'">Contact</a>
+      </nav>
+
+  </header>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <Header/>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue';
+</script>
