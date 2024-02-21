4834c858ed7fc64b1c951b7302ab143074efd63e
KaynoxDev <r.chauvard18@gmail.com>
2024-02-21 10:18:38 +0100

    structure du footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <footer>
+            <small>Dernière mise à jour le 21/02/2024</small>  
+            <section>
+                <a href="https://github.com/kaynoxdev" target="_blank" ref=""><img src="../assets/img/git.png" alt="icon de github"></a>
+                <a href="https://github.com/kaynoxdev"><img src="../assets/img/linkd.png" alt="icon de linkedin"></a>
+
+            </section>
+            <small> &copy; 2024 by KaynoxDev</small>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+
+footer {
+  background-color: #043444;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1230px;
+  margin-top: 100px;
+  height: 50px;
+  align-items: center;
+  border-top: 0.1rem solid #0484b7;
+  overflow: auto;
+  color: var(--secondary-color);
+}
+
+    img {
+        width: 30px;
+        height: 30px;
+        margin-left: 10px;
+    }
+</style>
--- src/components/aboutMe.vue
+++ src/components/aboutMe.vue
@@ -1,54 +1,66 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
-        <div id="aboutMe">
-        <section id="me">
-              <img src="../assets/img/me.png" alt="">
-            <p>Romain Chauvard</p>
-            <p>Développeur Web Junior</p>
-            <ul>
-              <li>
-                <img src="../assets/img/html.png" alt="logo du language html">
-                <div class="skills"><span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star"></span>
-<span class="fa fa-star"></span></div>
-              </li>
-              <li>
-                <img src="../assets/img/css.png" alt="logo du language css">
-                <div class="skills"><span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star"></span>
-<span class="fa fa-star"></span></div>
-              </li>
-              <li>
-                <img src="../assets/img/js.png" alt="logo du language js">
-                <div class="skills"><span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star"></span>
-<span class="fa fa-star"></span>
-<span class="fa fa-star"></span></div>
-              </li>
-            </ul>
-            
-        </section>
-        <section id="myHistory">
-          <h2>Présentation</h2>
-          <p>
-Bonjour, je suis un passionné de l’informatique depuis toujours. Mon parcours professionnel est assez diversifié et riche en expériences. J’ai commencé ma carrière dans la cuisine à l’âge de 16 ans, mais j’ai dû arrêter à 20 ans en raison des horaires difficiles à concilier avec une vie de famille.
-
-Par la suite, j’ai exploré le domaine de la cryptomonnaie en faisant du streaming. J’ai également travaillé chez Ledger, où j’ai contribué à la fabrication de leurs portefeuilles crypto. J’ai ensuite fait de l’intérim avant de devenir cariste.
-
-Malgré ces différentes expériences, ma passion pour l’informatique n’a jamais faibli. Étant père de quatre enfants, je n’avais jamais pris le temps de me consacrer pleinement à cette passion.</p>
-<p>Aujourd’hui, j’ai décidé de sauter le pas et de me lancer dans la carrière qui me correspond le mieux : le développement web. Je suis enthousiaste à l’idée d’évoluer dans ce domaine et j’ai hâte de voir où cette nouvelle aventure me mènera.</p>
-        </section>
-      </div>
+  <div id="aboutMe">
+    <section id="me">
+      <img src="../assets/img/me.png" alt="" />
+      <p>Romain Chauvard</p>
+      <p>Développeur Web Junior</p>
+      <ul>
+        <li>
+          <img src="../assets/img/html.png" alt="logo du language html" />
+          <div class="skills">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+        </li>
+        <li>
+          <img src="../assets/img/css.png" alt="logo du language css" />
+          <div class="skills">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+        </li>
+        <li>
+          <img src="../assets/img/js.png" alt="logo du language js" />
+          <div class="skills">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section id="myHistory">
+      <h2>Présentation</h2>
+      <p>
+        Bonjour, je suis un passionné de l’informatique depuis toujours. Mon parcours professionnel
+        est assez diversifié et riche en expériences. J’ai commencé ma carrière dans la cuisine à
+        l’âge de 16 ans, mais j’ai dû arrêter à 20 ans en raison des horaires difficiles à concilier
+        avec une vie de famille. Par la suite, j’ai exploré le domaine de la cryptomonnaie en
+        faisant du streaming. J’ai également travaillé chez Ledger, où j’ai contribué à la
+        fabrication de leurs portefeuilles crypto. J’ai ensuite fait de l’intérim avant de devenir
+        cariste. Malgré ces différentes expériences, ma passion pour l’informatique n’a jamais
+        faibli. Étant père de quatre enfants, je n’avais jamais pris le temps de me consacrer
+        pleinement à cette passion.
+      </p>
+      <p>
+        Aujourd’hui, j’ai décidé de sauter le pas et de me lancer dans la carrière qui me correspond
+        le mieux : le développement web. Je suis enthousiaste à l’idée d’évoluer dans ce domaine et
+        j’ai hâte de voir où cette nouvelle aventure me mènera.
+      </p>
+    </section>
+  </div>
 </template>
 
 <style scoped>
-
 #aboutMe {
   display: flex;
   flex-direction: column;
@@ -72,7 +84,6 @@ Malgré ces différentes expériences, ma passion pour l’informatique n’a ja
   margin-right: 20px;
 }
 
-
 #me img {
   border-radius: 50%;
   width: 100px;
@@ -80,26 +91,23 @@ Malgré ces différentes expériences, ma passion pour l’informatique n’a ja
   margin-right: 20px;
 }
 
-section h2 ,section p{
+section h2,
+section p {
   display: flex;
   justify-content: center;
   padding: 5px;
   color: var(--secondary-color);
   letter-spacing: 2px;
   color: #04a3e3;
-  
-
 }
-
-
 
 section h2 {
   text-decoration: underline;
 }
 
 section img {
-width: 50px;
-height: 50px;
+  width: 50px;
+  height: 50px;
 }
 
 section {
@@ -107,7 +115,7 @@ section {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #043444;  
+  background-color: #043444;
   box-shadow: 5px 5px 20px #bcc9ce;
   width: 80%;
   border-radius: 1%;
@@ -139,5 +147,4 @@ section {
 .skills {
   border-radius: 10%;
 }
-
 </style>
